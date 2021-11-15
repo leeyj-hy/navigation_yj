328df628901020453fb07d127d5a0ee6f5ba5db7
leeyj-hy <dldydwo981201@gmail.com>
2021-11-16 02:34:20 +0900

    nav_goal changed to 18, add 5s counter

--- src/nav_goal.cpp
+++ src/nav_goal.cpp
@@ -1,6 +1,6 @@
 #include <ros/ros.h>
 #include "geometry_msgs/PoseStamped.h"
-
+int i=6;
 int main(int argc, char** argv)
 {
 	ros:: init(argc, argv, "pub_node");
@@ -9,23 +9,26 @@ int main(int argc, char** argv)
 	ros::Publisher goal_pose = nh.advertise<geometry_msgs::PoseStamped>("goal_pose", 10);
 
 	ros::Rate loop_rate(1);
-	geometry_msgs::PoseStamped goal_pose_msg;
+	geometry_msgs::PoseStamped goal_pose_yj;
 
-	goal_pose_msg.pose.position.x=0;
-	goal_pose_msg.pose.position.y=0;
-	goal_pose_msg.pose.position.z=0;
+	goal_pose_yj.pose.position.x=18;
+	goal_pose_yj.pose.position.y=18;
+	goal_pose_yj.pose.position.z=18;
 
 	ROS_INFO("nav_ready!");
 
 	while(ros::ok())
-	{
-		goal_pose.publish(goal_pose_msg);
+	{	i-=1;
+		if(i>0)
+			ROS_INFO("publish start in %d sec", i);
+		else
+		{
+			goal_pose.publish(goal_pose_yj);
 
-		ROS_INFO("goal_pose x : %d", goal_pose_msg.pose.position.x);
-		ROS_INFO("goal_pose y : %d", goal_pose_msg.pose.position.y);
-		ROS_INFO("goal_pose z : %d", goal_pose_msg.pose.position.z);
-	
+			ROS_INFO("goal_pose published!");
 
+		}
+		
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
